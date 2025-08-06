949e7d03fbc3a9f8f465ef323de37170ab0be4f9
Mikkel IR <mikkel.robinson@lego.com>
2025-08-06 15:04:27 +0200

    nginx

--- setup-nginx-certbot.sh
+++ setup-nginx-certbot.sh
@@ -24,10 +24,10 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:$APP_PORT;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }
 
@@ -40,10 +40,10 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:$APP_PORT;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }
 EOF
